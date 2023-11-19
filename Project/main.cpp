640528ced5c732a51eb21b0cba1d60e9b4843108
Alcamist <j.son@digipen.edu>
2023-11-19 23:46:14 +0900

    Apply c++ core guidline checker

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from Physics Project!\n";
+
+#include <Box2D/Box2D.h>
+#include <gsl/gsl>
+#include<iostream>
+
+int main() 
+{
+  int*p = nullptr;
+  std::cout<<p<<'\n';
+    
+
+    return 0;
 }
